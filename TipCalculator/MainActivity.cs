3e20d8b22e6da8cc8cfe96c63d4bebee2360dec6
Nick Moyer <nick.moyer@awh.net>
2018-12-24 11:18:53 -0500

    Created the app functionality

--- TipCalculator/MainActivity.cs
+++ TipCalculator/MainActivity.cs
@@ -12,12 +12,37 @@ namespace TipCalculator
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        EditText inputBill;
+        Button calculateButton;
+        TextView outputTip;
+        TextView outputTotal;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
 
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            calculateButton.Click += OncCalculateClick;
+        }
+
+        private void OncCalculateClick(object sender, EventArgs e)
+        {
+            string text = inputBill.Text;
+            double bill;
+
+            if (double.TryParse(text, out bill))
+            {
+                double tip = bill * 0.15;
+                double total = bill + tip;
+
+                outputTip.Text = tip.ToString("C");
+                outputTotal.Text = total.ToString("C");
+            }
         }
     }
 }
